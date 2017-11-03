a49b6694c30163b5291197aef2ceda8eb3d10776
cydparser <cydparser@gmail.com>
2017-11-02 17:25:43 -0700

    Remove init-projectile-test-suffix
    Projectile now uses the "Spec" suffix by default.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -283,14 +283,8 @@
   :diminish ""
   :init
   (progn
-    (defun init-projectile-test-suffix (project-type)
-      "Find default test files suffix based on PROJECT-TYPE."
-      (cond ((member project-type '(haskell-stack)) "Spec")
-            (t (projectile-test-suffix project-type))))
-
     (setq projectile-create-missing-test-files t
           projectile-mode-line nil
-          projectile-test-suffix-function #'init-projectile-test-suffix
           projectile-use-git-grep t)
     (make-variable-buffer-local 'projectile-tags-command)
     (projectile-mode)))
